e527e305c72ac24efa92cd7922b6edf03659d501
JotaGalera <javigalera94@gmail.com>
2019-08-26 17:28:39 +0200

    Cambio int - bigint user

--- database/migrations/2019_04_30_154002_create_ubicacion_user_table.php
+++ database/migrations/2019_04_30_154002_create_ubicacion_user_table.php
@@ -16,7 +16,7 @@ class CreateUbicacionUserTable extends Migration
       Schema::create('ubicacion_user', function (Blueprint $table) {
           $table->increments('id');
           $table->integer('ubicacion_id')->unsigned()->nullable();
-          $table->integer('user_id')->unsigned();
+          $table->biginteger('user_id')->unsigned();
           $table->foreign('ubicacion_id')->references('id')->on('ubicacion')->onDelete('cascade');
           $table->foreign('user_id')->references('id')->on('users');
           $table->timestamps();
